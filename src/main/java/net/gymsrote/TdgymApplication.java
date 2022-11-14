1c3f61a265988f50fc85a350351d98c726c21bc2
thanh141121 <84693464+thanh141121@users.noreply.github.com>
2022-11-15 02:00:50 +0700

    Update TdgymApplication.java

--- src/main/java/net/gymsrote/TdgymApplication.java
+++ src/main/java/net/gymsrote/TdgymApplication.java
@@ -21,58 +21,38 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
 
 import java.util.Map;
-
 @SpringBootApplication
-@EnableSwagger2
 public class TdgymApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(TdgymApplication.class, args);
+		
 	}
-	// @Bean
-	// PasswordEncoder passwordEncoder() {
-	// return new BCryptPasswordEncoder();
-	// }
+	
+//	@Bean
+//	PasswordEncoder passwordEncoder() {
+//		return new BCryptPasswordEncoder();
+//	}
+	
+//	@Bean
+//	CommandLineRunner run(UserService userService) {
+//		return args -> {
+//			userService.saveRole(new Role(EUserRole.ROLE_USER));
+//			userService.saveRole(new Role(EUserRole.ROLE_ADMIN));
+//			userService.saveUser(new User("ttt14.11.21@gmail.com", "ttt141121", "141121", "Thanh Trung Tran", "0338989824", new ArrayList<>()));
+//			userService.addRoleToUser("ttt141121", EUserRole.ROLE_ADMIN);
+//			userService.addRoleToUser("ttt141121", EUserRole.ROLE_USER);
+////			File file = new File("test.png");
+////			byte[] bytes = Files.readAllBytes(file.toPath());
+////			CloudResource temp = cloudResourceService.save(bytes);
+////			System.out.println(temp.getUrl());
+//		};
+//	}
 
-	@Bean
-	CommandLineRunner run(UserService userService) {
-		return args -> {
-			userService.saveRole(new Role(EUserRole.ROLE_USER));
-			userService.saveRole(new Role(EUserRole.ROLE_ADMIN));
-			userService.saveUser(new User("ttt14.11.21@gmail.com", "ttt141121", "141121",
-					"Thanh Trung Tran", "0338989824", new ArrayList<>()));
-			userService.addRoleToUser("ttt141121", EUserRole.ROLE_ADMIN);
-			userService.addRoleToUser("ttt141121", EUserRole.ROLE_USER);
-			// File file = new File("test.png");
-			// byte[] bytes = Files.readAllBytes(file.toPath());
-			// CloudResource temp = cloudResourceService.save(bytes);
-			// System.out.println(temp.getUrl());
-		};
-	}
 
-	@Bean
-	public Docket productApi() {
-		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("net.gymsrote")).build();
-	}
-	// @Bean
-	// PasswordEncoder passwordEncoder() {
-	// return new BCryptPasswordEncoder();
-	// }
-	// @Bean
-	// CommandLineRunner run(CloudResourceService cloudResourceService) {
-	// return args -> {
-	//// userService.saveRole(new RoleEntity(EUserRole.ROLE_USER));
-	//// userService.saveRole(new RoleEntity(EUserRole.ROLE_ADMIN));
-	//// userService.saveUser(new UserEntity("ttt14.11.21@gmail.com", "ttt141121", "141121", "Thanh
-	// Trung Tran", "0338989824", new ArrayList<>()));
-	//// userService.addRoleToUser("ttt141121", EUserRole.ROLE_ADMIN);
-	//// userService.addRoleToUser("ttt141121", EUserRole.ROLE_USER);
-	// File file = new File("test.png");
-	// byte[] bytes = Files.readAllBytes(file.toPath());
-	// CloudResource temp = cloudResourceService.save(bytes);
-	// System.out.println(temp.getUrl());
-	// };
-	// }
+
 }
