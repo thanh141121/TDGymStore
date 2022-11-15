f2153ced0a3a210e9eb5f429c26f123e2e004cb7
phamphongduycn123 <phamphongduycn123@gmail.com>
2022-11-15 10:04:31 +0700

    swagger with authoriation

--- src/main/java/net/gymsrote/config/SecurityConfig.java
+++ src/main/java/net/gymsrote/config/SecurityConfig.java
@@ -12,45 +12,61 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import lombok.RequiredArgsConstructor;
-// import net.gymsrote.security.CustomUserDetailsService;
-// import net.gymsrote.security.JwtAuthenticationEntryPoint;
-// import net.gymsrote.security.JwtAuthenticationFilter;
+
+//import net.gymsrote.security.CustomUserDetailsService;
+//import net.gymsrote.security.JwtAuthenticationEntryPoint;
+//import net.gymsrote.security.JwtAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+
 public class SecurityConfig {
+	
 	@Autowired
 	CustomAuthenticationEntryPoint authenticationExceptionHandling;
-
+	
 	@Autowired
 	CustomAccessDeniedHandler customAccessDeniedHandler;
-
+	
 	@Bean
-	protected BCryptPasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
-
+    protected BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+	
 	@Bean
-	protected AuthenticationManager authenticationManager(
-			AuthenticationConfiguration authenticationConfiguration) throws Exception {
-		return authenticationConfiguration.getAuthenticationManager();
-	}
-
-	@Bean
-	protected AuthTokenFilter authTokenFilter() {
+    protected AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+    	return authenticationConfiguration.getAuthenticationManager();
+    }    
+    
+    @Bean
+    protected AuthTokenFilter authTokenFilter() {
 		return new AuthTokenFilter();
 	}
+    
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    	http
+    		.cors()
+    		.and()
+	    	.csrf().disable();
+    	
+    	http
+	    	.authorizeRequests()
+			.antMatchers("/api/admin/login",
+						"/api/user/login", "/api/user/signup").permitAll()
+			.and()
+			.authorizeRequests()
+    		.antMatchers("/api/admin/**").hasRole("ADMIN")
+    		.and()
+    		.authorizeRequests()
+    		.antMatchers("/api/user/**").hasRole("USER")
+			.and()
+    		.authorizeRequests()
+    		.antMatchers("/**").permitAll();
 
-	@Bean
-	protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.cors().and().csrf().disable();
-		http.authorizeRequests()
-				.antMatchers("/api/admin/login", "/api/user/login", "/api/user/signup").permitAll()
-				.and().authorizeRequests().antMatchers("/api/admin/**").hasRole("ADMIN").and()
-				.authorizeRequests().antMatchers("/api/user/**").hasRole("USER").and()
-				.authorizeRequests().antMatchers("/**").permitAll();
-		http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-		return http.build();
-	}
+    	http.addFilterBefore(authTokenFilter(), UsernamePasswordAuthenticationFilter.class);
+    	return http.build();
+    }
+
 }
